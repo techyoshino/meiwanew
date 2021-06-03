231ec9793d97c4a3d24b2ce8ed88fc766943006b
techyoshino <83795891+techyoshino@users.noreply.github.com>
2021-06-03 17:51:14 +0900

    yoshino-2021-06-03

---
+++ wp-content/themes/lovebond-child/page-construction.php
@@ -0,0 +1,185 @@
+<?php
+/**
+ * The template for displaying pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage SketchThemes
+ * @since Lovebond Lite 1.0.0
+ */
+
+get_header(cont); ?>
+
+<!-- BEGIN: BLOG -->
+
+<section id="contents-all" class="contents-all">
+    <!-- BEGIN: CONTAINER -->
+   <div class="container post-wrap rpage_wrap">
+
+   		<!-- BEGIN:block-1  -->
+
+		<section class="block-1 contents_block">
+			
+				
+			<div class="cap">
+			いろんな要望に答え<br />
+			こんな機械を作ってきました
+			</div>	
+
+			
+			<div class="block-1_inner scroll_element scroll_off0">
+
+				<div class="content-wrap col-md-6 left-photo">
+				
+				<img src="<?php echo get_template_directory_uri(); ?>/images/1.jpg" />
+					<div class="clearfix"></div>
+				</div>
+			
+
+				<div class="col-md-6">
+
+					<div class="right-txt">
+					
+					<div class="right-txt-cap">
+						<h3>機械の名前</h3>
+					</div>
+
+					<div class="right-txt-in">
+						<p>
+						テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+						</p>
+					</div>	
+
+					</div>	
+				</div>
+			</div>
+			<!-- //block-1_inner -->
+
+
+			<div class="block-1_inner scroll_element scroll_off0">
+
+				<div class="content-wrap col-md-6">
+
+					<div class="left-txt">
+						
+						<div class="left-txt-cap">
+							<h3>機械の名前</h3>
+						</div>
+
+						<div class="left-txt-in">
+							<p>
+							テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+							</p>
+						</div>	
+
+					</div>	
+				
+				</div>
+
+				<div class="col-md-6 right-photo">
+
+					<img src="<?php echo get_template_directory_uri(); ?>/images/1.jpg" />
+					<div class="clearfix"></div>
+
+				</div>
+			</div>
+			<!-- //block-1_inner -->
+
+
+			<div class="block-1_inner scroll_element scroll_off0">
+
+				<div class="content-wrap col-md-6 left-photo">
+				
+				<img src="<?php echo get_template_directory_uri(); ?>/images/1.jpg" />
+					<div class="clearfix"></div>
+				</div>
+			
+
+				<div class="col-md-6">
+
+					<div class="right-txt">
+					
+					<div class="right-txt-cap">
+						<h3>機械の名前</h3>
+					</div>
+
+					<div class="right-txt-in">
+						<p>
+						テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+						</p>
+					</div>	
+
+					</div>	
+				</div>
+			</div>
+			<!-- //block-1_inner -->
+
+
+			<div class="block-1_inner scroll_element scroll_off0">
+
+				<div class="content-wrap col-md-6">
+
+					<div class="left-txt">
+						
+						<div class="left-txt-cap">
+							<h3>機械の名前</h3>
+						</div>
+
+						<div class="left-txt-in">
+							<p>
+							テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+							</p>
+						</div>	
+
+					</div>	
+				
+				</div>
+
+				<div class="col-md-6 right-photo">
+
+					<img src="<?php echo get_template_directory_uri(); ?>/images/1.jpg" />
+					<div class="clearfix"></div>
+
+				</div>
+			</div>
+			<!-- //block-1_inner -->
+
+		</section>
+		<!-- END:block-1  -->
+
+		<!-- BEGIN:block-2  -->
+
+		<section class="block-2 contents_block">
+
+			<div class="cap">お取引一覧</div>
+			
+			<ul>
+				<li>朝日電装株式会社</li>
+				<li>株式会社アンセイ</li>
+				<li>株式会社今仙電機製作所</li>
+				<li>カーツ株式会社</li>
+				<li>株式会社三星電機製作所</li>
+				<li>清水建設株式会社</li>
+				<li>住友電装株式会社</li>
+				<li>住友理工株式会社</li>
+				<li>タイム技研株式会社</li>
+				<li>大洋産業株式会社</li>
+				<li>大和冷機工業株式会社</li>
+				<li>タカラスタンダード株式会社</li>
+				<li>玉野化成株式会社</li>
+				<li>DNP田村プラスチック株式会社</li>
+					
+			</ul>	
+
+		</section>
+
+		<!-- BEGIN:block-2  -->
+
+	</div>
+</section>
+<!-- END: BLOG -->
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/lovebond-child/page-flow.php
@@ -0,0 +1,138 @@
+<?php
+/**
+ * The template for displaying pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage SketchThemes
+ * @since Lovebond Lite 1.0.0
+ */
+
+get_header(cont); ?>
+
+<!-- BEGIN: BLOG -->
+
+<section id="contents-all" class="contents-all">
+    <!-- BEGIN: CONTAINER -->
+   <div class="container post-wrap rpage_wrap">
+
+	<!-- BEGIN:block-1  -->
+
+		<section class="block-1 contents_block">
+			
+				
+			<div class="cap">
+			専用機械のセットアップ、<br />
+			その全行程をお見せします
+			</div>	
+
+			<div class="block-1_inner scroll_element scroll_off0">
+
+				<div class="flow_line"></div>
+					<div class="flow_list">
+					<div class="flow_right">
+						<div class="flow_title"><p class="">01</p><p class="">お問い合わせ</p></div>
+
+						<div class="flow_txt">
+						「加工方法がわからない」「どうやったら加工できるだろう」<br />
+						そんな悩みを抱えたお客さまより、メールや電話でお問い合わせをいただきます。
+							<div class="flow_contact">
+								<p>ご相談は無料</p>
+								<a href="#">お問い合わせ</a>
+							</div>	
+						</div>
+
+
+					</div>
+				</div>
+
+
+				<div class="flow_line"></div>
+					<div class="flow_list">
+					<div class="flow_right">
+						<div class="flow_title"><p class="">02</p><p class="">打ち合わせ</p></div>
+						<div class="flow_txt">
+						当社の担当から、提案を兼ねた打ち合わせを行います。<br />
+						場合によっては現地にお伺いすることもあります。
+						</div>	
+					</div>
+				</div>
+
+				<div class="flow_line"></div>
+					<div class="flow_list">
+					<div class="flow_right">
+						<div class="flow_title"><p class="">03</p><p class="">設計</p></div>
+						<div class="flow_txt">
+						ご要望をお聞きし、機械の特徴を考慮しながら、強度などの計算やデータの解析、模型製作、CAD設計などを行います。
+						</div>	
+					</div>
+				</div>
+
+
+				<div class="flow_line"></div>
+					<div class="flow_list">
+					<div class="flow_right">
+						<div class="flow_title"><p class="">04</p><p class="">お見積り・ご契約</p></div>
+						<div class="flow_txt">
+						必要な部品、自社工場での組立シミュレーションなどを実施した上で、詳細なお見積もりを提示させていただきます。
+						</div>	
+					</div>
+				</div>
+
+				<div class="flow_line"></div>
+					<div class="flow_list">
+					<div class="flow_right">
+						<div class="flow_title"><p class="">05</p><p class="">組み立て開始</p></div>
+						<div class="flow_txt">
+						自社工場にて部品や治具の加工、製造、組み立てを開始します。
+						<img src="<?php echo get_template_directory_uri(); ?>/images/flow-1.jpg" />
+						</div>	
+					</div>
+				</div>
+
+				<div class="flow_line"></div>
+					<div class="flow_list">
+					<div class="flow_right">
+						<div class="flow_title"><p class="">06</p><p class="">テスト・完成</p></div>
+						<div class="flow_txt">
+						マシンが正常に稼働するか、念入りにテストを行います。
+						</div>	
+					</div>
+				</div>
+
+				<div class="flow_line"></div>
+					<div class="flow_list">
+					<div class="flow_right">
+						<div class="flow_title"><p class="">07</p><p class="">納品</p></div>
+						<div class="flow_txt">
+						ご指定の場所に設置し、問題なく稼働するようにセッティングするまでが納品です。
+						<img src="<?php echo get_template_directory_uri(); ?>/images/flow-2.jpg" />
+						</div>	
+					</div>
+				</div>
+
+				<div class="flow_line"></div>
+					<div class="flow_list">
+					<div class="flow_right">
+						<div class="flow_title"><p class="">08</p><p class="">運用・サポート</p></div>
+						<div class="flow_txt">
+						オーダーメイドマシンの製造元は明和エンジニアリング。設置後の運用保守やサポートもお任せください
+						</div>	
+					</div>
+				</div>
+
+
+
+			</div>
+			
+		</section>
+		<!-- END:block-1  -->
+	</div>
+
+</section>
+<!-- END: BLOG -->
+
+<?php get_footer(); ?>
