dc4d02decbab738186f997845137a4905b903937
fuchigami7188 <fuchigami@techtrage.co.jp>
2021-06-09 10:32:57 +0900

    20210609-fu-ver2

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@
 
 // ** MySQL 設定 - この情報はホスティング先から入手してください。 ** //
 /** WordPress のためのデータベース名 */
-define( 'DB_NAME', 'meiwa-engineering' );
+define( 'DB_NAME', 'meiwa' );
 
 /** MySQL データベースのユーザー名 */
 define( 'DB_USER', 'root' );
